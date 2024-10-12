811e71d8aafff27f6ff49e167e7fc571785fe335
ferro96 <davidac099@gmail.com>
2024-10-11 21:45:37 -0600

    Add relation between courses and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: 'admin@admin.com', password: 'Password1234', password_confirmation: 'Password1234')
+
 30.times do
   Course.create!([ {
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   } ])
 end
