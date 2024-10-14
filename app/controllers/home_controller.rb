a8e28268534812535b55cc549f40be1469978ba0
ferro96 <davidac099@gmail.com>
2024-10-14 12:19:44 -0600

    Style home page and add courses to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index ]
   def index
+    @courses = Course.all.limit(4)
+    @latest_courses = Course.all.limit(4).order(created_at: :desc)
   end
 end
