19c210555ce7a28e93a3a75c2d1e800c52e8aae0
ferro96 <davidac099@gmail.com>
2024-10-20 12:16:30 -0600

    Added ransack to search header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   allow_browser versions: :modern
+
+  before_action :set_global_variables, if: :user_signed_in?
+  # navbar search
+  def set_global_variables
+    @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+  end
 end
