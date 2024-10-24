4d7a178731cf9bdc4b6ffd2e315105cf7ef7a9f3
ferro96 <davidac099@gmail.com>
2024-10-24 16:41:37 -0600

    WIP add route and edit page for roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,16 @@ class UsersController < ApplicationController
     @q = User.ransack(params[:q])
     @users = @q.result(distinct: true)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User #{@user.email} was successfully updted'
+    else
+      render :edit
+    end
+  end
 end
