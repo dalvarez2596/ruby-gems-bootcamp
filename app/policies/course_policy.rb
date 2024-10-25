5cf5666cad6c389979b361f2edcda4f85222a2ca
ferro96 <davidac099@gmail.com>
2024-10-25 15:36:22 -0600

    Added authorize for course actions

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -13,6 +13,22 @@ class CoursePolicy < ApplicationPolicy
   end
 
   def edit?
-    @user.has_role?(:admin)
+    @user.has_role?(:admin) or @record.user == @user
+  end
+
+  def update?
+    @user.has_role?(:admin) or @record.user == @user
+  end
+
+  def new?
+    @user.has_role?(:teacher)
+  end
+
+  def create?
+    @user.has_role?(:teacher)
+  end
+
+  def destroy?
+    @user.has_role?(:admin) or @record.user == @user
   end
 end
