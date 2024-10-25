acd7e39f597f21a0b021ef930bf7215589968710
ferro96 <davidac099@gmail.com>
2024-10-25 11:21:57 -0600

    Added Course Policy and include it in Course

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,21 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   allow_browser versions: :modern
 
+  include Pundit::Authorization
+  protect_from_forgery
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   include PublicActivity::StoreController # save current_user using gem public activity
 
   before_action :set_global_variables, if: :user_signed_in?
   # navbar search
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+  end
+
+  private
+  def user_not_authorized # pundit
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_back_or_to(root_path)
   end
 end
