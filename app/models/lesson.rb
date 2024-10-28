bc48d89316785c8e0219279a873a382c1c9efafb
ferro96 <davidac099@gmail.com>
2024-10-28 08:40:18 -0600

    Added public activity to lessons, create lessons inside courses

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,14 @@ class Lesson < ApplicationRecord
   belongs_to :course
   validates :title, :content, :course, presence: true
 
+  def to_s
+    title
+  end
+
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  include PublicActivity::Model
+  # tracked
+  tracked owner: Proc.new { |controller, model| controller.current_user }
 end
